498274a3dfab646a038d81d2cf83dc2c0794081d
Eduardo manuel Portela da silva <eportela@c2r4s7.42lisboa.com>
2023-12-22 13:16:33 +0000

    added Dog and Cat semi implementation

--- ex00/src/Cat.cpp
+++ ex00/src/Cat.cpp
@@ -1,1 +1,13 @@
 #include "../includes/Cat.hpp"
+
+Cat::Cat() : public Animal("Cat") {}
+
+Cat::~Cat() {
+    std::cout << "A Cat has been destroyed, no cats were hurt in this process" << std::endl;
+}
+
+Cat::void makeSound() const {}
+
+Cat::Cat(const Cat& other) {}
+
+Cat::Cat& operator=(const Cat& assign) {}
--- ex00/src/Dog.cpp
+++ ex00/src/Dog.cpp
@@ -1,1 +1,13 @@
 #include "../includes/Dog.hpp"
+
+Dog::Dog() : public Animal("Dog") {}
+
+Dog::~Dog() {
+    std::cout << "A dog has been destroyed, no dogs were hurt in this process" << std::endl;
+}
+
+Dog::void makeSound() const {}
+
+Dog::Dog(const Dog& other) {}
+
+Dog::Dog& operator=(const Dog& assign) {}
